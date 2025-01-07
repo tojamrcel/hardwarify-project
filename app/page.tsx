801de54c0c658747aa023381590e8ae9f0463469
Nadia Kuna <marcelgala42@gmail.com>
2025-01-07 20:03:29 +0100

    style: add styles for button in hero section

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,12 @@ export default function Home() {
       <div className="flex flex-col items-center gap-2">
         <h2 className="text-center text-4xl font-bold">TEXT GOES THERE</h2>
         <span className="w-full text-center font-semibold">xx% off</span>
-        <Link href="/">Buy now</Link>
+        <Link
+          href="/"
+          className="rounded-md bg-red-600 px-4 py-2 font-semibold text-stone-100 transition-colors duration-300 hover:bg-red-700"
+        >
+          Buy now
+        </Link>
       </div>
       <div>
         <img src="telefon.png" alt="iphone" className="max-h-96" />

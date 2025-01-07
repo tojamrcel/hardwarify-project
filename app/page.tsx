cc6ccb578a6959389708b602768d49225a93705e
Nadia Kuna <marcelgala42@gmail.com>
2025-01-07 20:00:28 +0100

    style: add border-radius to hero section in home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <section className="grid w-full grid-cols-2 place-items-center justify-center bg-white-second px-4 py-8">
+    <section className="grid w-full grid-cols-2 place-items-center justify-center rounded-lg bg-white-second px-4 py-8">
       <div className="flex flex-col items-center gap-2">
         <h2 className="text-center text-4xl font-bold">TEXT GOES THERE</h2>
-        <span className="w-full text-center font-semibold">20% off</span>
+        <span className="w-full text-center font-semibold">xx% off</span>
         <Link href="/">Buy now</Link>
       </div>
       <div>

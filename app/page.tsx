13a8e7ab034cdcc282d9cbe723bb673bd487100f
tojamrcel <marcelgala42@gmail.com>
2025-01-08 23:57:09 +0100

    style: change color of h2 in hero section

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,9 @@ export default function Page() {
     <>
       <section className="grid w-full grid-cols-2 place-items-center justify-center rounded-lg bg-white-second px-4 py-8">
         <div className="flex flex-col items-center gap-2">
-          <h2 className="text-center text-4xl font-bold">TEXT GOES THERE</h2>
+          <h2 className="text-center text-4xl font-bold text-stone-800">
+            TEXT GOES THERE
+          </h2>
           <span className="w-full text-center font-semibold">xx% off</span>
           <Link
             href="/"

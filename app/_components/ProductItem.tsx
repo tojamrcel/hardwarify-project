303013ce576df54c60a0f20c71592c7ef7f8d44d
tojamrcel <marcelgala42@gmail.com>
2025-01-12 00:02:54 +0100

    refactor,style: change styles for filters and create a seperate component for product item

---
+++ app/_components/ProductItem.tsx
@@ -0,0 +1,16 @@
+function ProductItem() {
+  return (
+    <a href="#">
+      <div className="group relative flex h-36 w-36 flex-col items-center justify-center overflow-hidden rounded-md bg-white-second transition-transform duration-300 hover:scale-105">
+        <div className="flex items-center justify-center">
+          <img className="w-20" src="airpods.png" alt="airpods" />
+        </div>
+        <p className="absolute bottom-0 w-full origin-bottom scale-y-0 bg-gray-400 p-1 text-center font-semibold text-stone-100 transition-all duration-200 group-hover:scale-100">
+          AirPods Pro
+        </p>
+      </div>
+    </a>
+  );
+}
+
+export default ProductItem;
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import ProductItem from "../_components/ProductItem";
 
 export const metadata: Metadata = {
   title: "Products",
@@ -15,24 +16,36 @@ function Page() {
         >
           Category
         </label>
-        <div className="ml-3 flex flex-col">
+        <div className="ml-3 mt-1 flex flex-col gap-2">
           <div className="flex items-center gap-3">
             <input type="checkbox" name="category" value="smartphones" />
             <p className="text-md text-gray-700">Smartphones</p>
           </div>
+          <div className="flex items-center gap-3">
+            <input type="checkbox" name="category" value="headphones" />
+            <p className="text-md text-gray-700">Headphones</p>
+          </div>
+          <div className="flex items-center gap-3">
+            <input type="checkbox" name="category" value="pc-gear" />
+            <p className="text-md text-gray-700">Gear</p>
+          </div>
+          <div className="flex items-center gap-3">
+            <input type="checkbox" name="category" value="gaming" />
+            <p className="text-md text-gray-700">Gaming</p>
+          </div>
         </div>
       </section>
-      <section className="grid grid-cols-5 gap-32">
-        <a href="#">
-          <div className="group relative flex h-36 w-36 flex-col items-center justify-center overflow-hidden rounded-md bg-white-second transition-transform duration-300 hover:scale-105">
-            <div className="flex items-center justify-center">
-              <img className="w-20" src="airpods.png" alt="airpods" />
-            </div>
-            <p className="absolute bottom-0 w-full origin-bottom scale-y-0 bg-gray-400 p-1 text-center font-semibold text-stone-100 transition-all duration-200 group-hover:scale-100">
-              AirPods Pro
-            </p>
-          </div>
-        </a>
+      <section className="grid grid-cols-5 gap-x-32 gap-y-8">
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
       </section>
     </section>
   );

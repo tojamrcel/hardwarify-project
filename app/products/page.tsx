02f3254c616b876b7db3d380f8cbf3b0a2b4c36b
tojamrcel <marcelgala42@gmail.com>
2025-01-11 23:57:46 +0100

    feat: create filters section in product page

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -6,8 +6,22 @@ export const metadata: Metadata = {
 
 function Page() {
   return (
-    <section className="flex justify-center">
-      <section></section>
+    <section className="flex justify-center gap-24">
+      <section className="rounded-md bg-white-second p-8 px-10">
+        <h2 className="text-xl font-bold text-gray-700">Filters</h2>
+        <label
+          htmlFor="category"
+          className="ml-2 text-lg font-semibold text-gray-700"
+        >
+          Category
+        </label>
+        <div className="ml-3 flex flex-col">
+          <div className="flex items-center gap-3">
+            <input type="checkbox" name="category" value="smartphones" />
+            <p className="text-md text-gray-700">Smartphones</p>
+          </div>
+        </div>
+      </section>
       <section className="grid grid-cols-5 gap-32">
         <a href="#">
           <div className="group relative flex h-36 w-36 flex-col items-center justify-center overflow-hidden rounded-md bg-white-second transition-transform duration-300 hover:scale-105">

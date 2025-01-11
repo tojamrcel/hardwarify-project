5545cba1e8a970f6b35f8d0a1a12161eaa9480ac
tojamrcel <marcelgala42@gmail.com>
2025-01-11 23:04:22 +0100

    refactor: create new component for recommended box

--- app/_components/HomeSection.tsx
+++ app/_components/HomeSection.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import RecommendedProducts from "./RecommendedProducts";
 
 function HomeSection({ textPlace }: { textPlace: "left" | "right" }) {
   return (
@@ -43,53 +44,7 @@ function HomeSection({ textPlace }: { textPlace: "left" | "right" }) {
           </>
         )}
       </section>
-      <section className="mt-10 grid grid-cols-3 gap-8">
-        <div className="grid grid-cols-2 rounded-md bg-white-second px-4 py-2">
-          <div className="flex w-full items-center justify-center">
-            <img src="airpods.png" alt="xxx" className="max-h-48 w-1/2" />
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            <h3 className="font-bold text-stone-800">Product</h3>
-            <span className="font-semibold">$300</span>
-            <a
-              href="#"
-              className="mt-1 border-b-2 border-transparent px-0.5 pb-[-0.25rem] pt-1 font-semibold text-stone-500 transition-colors duration-100 hover:border-stone-500"
-            >
-              Go to product
-            </a>
-          </div>
-        </div>
-        <div className="grid grid-cols-2 rounded-md bg-white-second px-4 py-2">
-          <div className="flex w-full items-center justify-center">
-            <img src="airpods.png" alt="xxx" className="max-h-48 w-1/2" />
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            <h3 className="font-bold text-stone-800">Product</h3>
-            <span className="font-semibold">$300</span>
-            <a
-              href="#"
-              className="mt-1 border-b-2 border-transparent px-0.5 pb-[-0.25rem] pt-1 font-semibold text-stone-500 transition-colors duration-100 hover:border-stone-500"
-            >
-              Go to product
-            </a>
-          </div>
-        </div>
-        <div className="grid grid-cols-2 rounded-md bg-white-second px-4 py-2">
-          <div className="flex w-full items-center justify-center">
-            <img src="airpods.png" alt="xxx" className="max-h-48 w-1/2" />
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            <h3 className="font-bold text-stone-800">Product</h3>
-            <span className="font-semibold">$300</span>
-            <a
-              href="#"
-              className="mt-1 border-b-2 border-transparent px-0.5 pb-[-0.25rem] pt-1 font-semibold text-stone-500 transition-colors duration-100 hover:border-stone-500"
-            >
-              Go to product
-            </a>
-          </div>
-        </div>
-      </section>
+      <RecommendedProducts />
     </>
   );
 }

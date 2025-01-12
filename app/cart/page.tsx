052147e51e947c4814843790a60868f318c6f3f0
tojamrcel <marcelgala42@gmail.com>
2025-01-12 23:47:20 +0100

    feat: add first elements to cart page

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -5,7 +5,29 @@ export const metadata: Metadata = {
 };
 
 function Page() {
-  return <div>Cart</div>;
+  return (
+    <section className="grid-cols-2">
+      <h1 className="text-4xl font-bold text-gray-700">Cart</h1>
+      <div className="p-4">
+        <div className="grid max-w-5xl grid-cols-[8rem_3fr] items-center justify-center gap-4 rounded-lg bg-white-second px-4 py-2">
+          <img
+            src="airpods.png"
+            alt="airpods"
+            className="h-24 w-24 self-center justify-self-center"
+          />
+          <div>
+            <p className="text-lg font-semibold text-gray-700">AirPods Pro</p>
+            <input
+              type="number"
+              className="mt-1 w-8 rounded-lg px-1 py-0.5 text-center"
+              min="1"
+              max="10"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Page;

0ae5b6d02b2b3f2248ab3bf521df6a8a6ed3440a
tojamrcel <marcelgala42@gmail.com>
2025-01-14 23:02:57 +0100

    style: change styles for product page

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -5,7 +5,7 @@ function Page() {
         <div className="flex items-center justify-center px-4 py-4">
           <img src="../telefon.png" alt="Iphone 16 Pro" className="w-96" />
         </div>
-        <div className="flex w-96 flex-col gap-4 p-4">
+        <div className="flex w-96 flex-col gap-6 p-4">
           <h2 className="text-4xl font-bold text-gray-700">PRODUCT NAME</h2>
           <p className="text-gray-700">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, vel
@@ -13,14 +13,9 @@ function Page() {
             voluptates illo quis expedita hic quibusdam eum et dolorem quia
             fugit non dolore.
           </p>
+          <span className="text-xl font-bold text-gray-700">499$</span>
           <div className="flex items-center gap-2">
-            <input
-              type="number"
-              className="mt-1 h-6 w-6 rounded-lg px-1 py-0.5 text-center font-semibold text-gray-700 focus:outline-none"
-              min="1"
-              max="10"
-            />
-            <button className="rounded-md bg-red-600 px-2 py-1 font-semibold text-stone-100 transition-colors duration-150 hover:bg-red-700">
+            <button className="rounded-md bg-red-600 px-3 py-1 font-semibold text-stone-100 transition-colors duration-150 hover:bg-red-700">
               Add to cart
             </button>
           </div>

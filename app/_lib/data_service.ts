3fda9c4c90a8b6aa7d09718074b52938a8d43207
tojamrcel <marcelgala42@gmail.com>
2025-01-16 22:04:19 +0100

    feat: implement type checking in getProducts function

--- app/_lib/data_service.ts
+++ app/_lib/data_service.ts
@@ -1,6 +1,16 @@
 import { supabase } from "./supabase";
 
-export async function getProducts() {
+interface Products {
+  id: number;
+  product_name: string;
+  regular_price: number;
+  discount: number | null;
+  description: string;
+  category: string;
+  image: string;
+}
+
+export async function getProducts(): Promise<Products> {
   const { data, error } = await supabase.from("products").select("*").single();
 
   if (error) console.error(error);

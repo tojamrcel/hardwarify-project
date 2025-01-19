db5de5d0707eb0f8d8cdef2041b5561de76a1c07
tojamrcel <marcelgala42@gmail.com>
2025-01-19 22:23:11 +0100

    refactor: change img element in ProductItem component into next Image component to improve performance of website

--- app/_components/ProductItem.tsx
+++ app/_components/ProductItem.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { Product } from "../_types/types";
+import Image from "next/image";
 
 function ProductItem({ product }: { product: Product }) {
   const { id, product_name: name, image } = product;
@@ -8,7 +9,13 @@ function ProductItem({ product }: { product: Product }) {
     <Link href={`/products/${id}`}>
       <div className="group relative flex h-36 w-36 flex-col items-center justify-center overflow-hidden rounded-md bg-white-second shadow-sm transition-transform duration-300 hover:scale-105">
         <div className="flex items-center justify-center">
-          <img className="w-24 rounded-full" src={image} alt={name} />
+          <Image
+            className="w-24 rounded-full"
+            src={image}
+            width={96}
+            height={96}
+            alt={name}
+          />
         </div>
         <p className="absolute bottom-0 w-full origin-bottom scale-y-0 bg-gray-400 p-1 text-center font-semibold text-stone-100 opacity-75 transition-all duration-200 group-hover:scale-100">
           {name}
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "oluekkawuggalugtvbig.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/products_images/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;

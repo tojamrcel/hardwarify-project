721399ae6c65ff121e74e4f4b54dd1568c9e7618
tojamrcel <marcelgala42@gmail.com>
2025-01-21 20:29:04 +0100

    feat: implement logic to avoid repeating products on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 import HomeSection from "./_components/HomeSection";
 import RecommendedProducts from "./_components/RecommendedProducts";
-import {
-  getBestsellers,
-  getLimitedProductsByCategory,
-} from "./_lib/data_service";
+import { getBestsellers, getProductsByCategory } from "./_lib/data_service";
 import { Product, ProductWithDiscount } from "./_types/types";
 
 function sortByDiscount(products: Product[]): ProductWithDiscount[] | [] {
@@ -23,15 +20,38 @@ function sortByDiscount(products: Product[]): ProductWithDiscount[] | [] {
   );
 }
 
+function removeRepeatingProducts(
+  category: ProductWithDiscount[],
+  bestsellers: ProductWithDiscount[],
+): ProductWithDiscount[] | [] {
+  if (!category || !bestsellers) return [];
+
+  return category
+    .map((product) => {
+      const isBestseller = Boolean(
+        bestsellers.find((bestseller) => bestseller.id === product.id),
+      );
+      if (isBestseller) return null;
+      return product;
+    })
+    .filter((product) => product !== null);
+}
+
 export default async function Page() {
   const bestsellers = await getBestsellers();
   const sortedBestsellers = sortByDiscount(bestsellers);
 
-  const gaming = await getLimitedProductsByCategory(4, "gaming");
-  const sortedGaming = sortByDiscount(gaming);
+  const gaming = await getProductsByCategory("gaming");
+  const sortedGaming = removeRepeatingProducts(
+    sortByDiscount(gaming),
+    sortedBestsellers,
+  );
 
-  const mobile = await getLimitedProductsByCategory(4, "mobile device");
-  const sortedMobile = sortByDiscount(mobile);
+  const mobile = await getProductsByCategory("mobile device");
+  const sortedMobile = removeRepeatingProducts(
+    sortByDiscount(mobile),
+    sortedBestsellers,
+  );
   return (
     <>
       <h2 className="mb-2 py-2 text-center text-5xl font-bold uppercase tracking-wider text-gray-700 underline">

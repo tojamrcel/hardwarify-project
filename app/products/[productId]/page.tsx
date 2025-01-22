703a20f21bed9088766027afe57e11719f8dd6fa
tojamrcel <marcelgala42@gmail.com>
2025-01-22 14:48:43 +0100

    feat: implement real data to product page

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -1,23 +1,35 @@
-function Page() {
+import { getProductById } from "@/app/_lib/data_service";
+import Image from "next/image";
+
+async function Page({ params }: { params: { productId: string } }) {
+  const { productId } = await params;
+  const {
+    product_name: name,
+    description,
+    regular_price: regularPrice,
+    discount,
+    image,
+  } = await getProductById(Number(productId));
+
   return (
-    <section className="mt-8 flex flex-col gap-4">
+    <section className="mt-8 flex max-h-[120rem] flex-col gap-4">
       <div className="flex items-center justify-evenly gap-8 rounded-lg bg-white-second shadow-md">
         <div className="flex items-center justify-center px-4 py-4">
-          <img
-            src="../iphone16pro.jpg"
+          <Image
+            src={image}
             alt="Iphone 16 Pro"
-            className="w-96 rounded-lg shadow-md"
+            width={384}
+            height={384}
+            className="rounded-lg shadow-md"
+            quality={95}
           />
         </div>
         <div className="flex w-96 flex-col gap-6 p-4">
-          <h2 className="text-4xl font-bold text-gray-700">PRODUCT NAME</h2>
-          <p className="text-gray-700">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, vel
-            est. Officia, cumque consequatur, iste explicabo inventore
-            voluptates illo quis expedita hic quibusdam eum et dolorem quia
-            fugit non dolore.
-          </p>
-          <span className="text-xl font-bold text-gray-700">499$</span>
+          <h2 className="text-4xl font-bold text-gray-700">{name}</h2>
+          <p className="text-gray-700">{description}</p>
+          <span className="text-xl font-bold text-gray-700">
+            {regularPrice - Number(discount)}$
+          </span>
           <div className="flex items-center gap-2">
             <button className="rounded-md bg-red-600 px-3 py-1 font-semibold text-stone-100 transition-colors duration-150 hover:bg-red-700">
               Add to cart

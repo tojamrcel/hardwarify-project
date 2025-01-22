1200d02e4165c16ac7a228d0e4d3347657bc3be2
tojamrcel <marcelgala42@gmail.com>
2025-01-22 16:26:28 +0100

    refactor: move CartSummary to outside component

---
+++ app/_components/CartSummary.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useCart } from "./CartContext";
+
+function CartSummary() {
+  const { cart } = useCart();
+
+  if (!cart.length) return null;
+
+  return (
+    <>
+      <h2 className="text-2xl font-bold text-gray-700">Summary</h2>
+      <div className="my-4 flex flex-col gap-2 px-4 text-gray-500">
+        <p>
+          <span className="font-semibold">Products:</span> xxx$
+        </p>
+        <p>
+          <span className="font-semibold">Shipping cost:</span> xxx$
+        </p>
+        <p>
+          <span className="font-semibold">Total:</span> xxx$
+        </p>
+        <button className="my-2 w-32 rounded-lg bg-red-600 px-2 py-2 font-semibold text-stone-100 transition-colors duration-300 hover:bg-red-700">
+          Checkout
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default CartSummary;
--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import CartList from "../_components/CartList";
+import CartSummary from "../_components/CartSummary";
 
 export const metadata: Metadata = {
   title: "Cart",
@@ -13,21 +14,7 @@ function Page() {
         <CartList />
       </div>
       <div>
-        <h2 className="text-2xl font-bold text-gray-700">Summary</h2>
-        <div className="my-4 flex flex-col gap-2 px-4 text-gray-500">
-          <p>
-            <span className="font-semibold">Products:</span> xxx$
-          </p>
-          <p>
-            <span className="font-semibold">Shipping cost:</span> xxx$
-          </p>
-          <p>
-            <span className="font-semibold">Total:</span> xxx$
-          </p>
-          <button className="my-2 w-32 rounded-lg bg-red-600 px-2 py-2 font-semibold text-stone-100 transition-colors duration-300 hover:bg-red-700">
-            Checkout
-          </button>
-        </div>
+        <CartSummary />
       </div>
     </section>
   );

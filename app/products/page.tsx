90d55bbab4ff593de2e55ec6d510b080ce5a485f
tojamrcel <marcelgala42@gmail.com>
2025-01-25 10:20:36 +0100

    feat: implement real categories in filters in products page

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -8,10 +8,11 @@ export const metadata: Metadata = {
 
 async function Page() {
   const products = await getProducts();
+  const categories = Array.from(new Set(products.map((prod) => prod.category)));
 
   return (
     <section className="flex justify-center gap-24">
-      <section className="rounded-md bg-white-second p-8 px-10 shadow-md">
+      <section className="w-1/4 rounded-md bg-white-second p-6 px-6 shadow-md">
         <h2 className="text-xl font-bold text-gray-700">Filters</h2>
         <label
           htmlFor="category"
@@ -19,23 +20,13 @@ async function Page() {
         >
           Category
         </label>
-        <div className="ml-3 mt-1 flex flex-col gap-2">
-          <div className="flex items-center gap-3">
-            <input type="checkbox" name="category" value="smartphones" />
-            <p className="text-md text-gray-700">Smartphones</p>
-          </div>
-          <div className="flex items-center gap-3">
-            <input type="checkbox" name="category" value="headphones" />
-            <p className="text-md text-gray-700">Headphones</p>
-          </div>
-          <div className="flex items-center gap-3">
-            <input type="checkbox" name="category" value="pc-gear" />
-            <p className="text-md text-gray-700">Gear</p>
-          </div>
-          <div className="flex items-center gap-3">
-            <input type="checkbox" name="category" value="gaming" />
-            <p className="text-md text-gray-700">Gaming</p>
-          </div>
+        <div className="ml-3 mt-1 flex w-full flex-col gap-2">
+          {categories.map((cat) => (
+            <div className="flex items-center gap-3" key={cat}>
+              <input type="checkbox" name="category" value={cat} />
+              <p className="text-md text-gray-700">{`${cat.length > 3 ? cat[0].toUpperCase() + cat.slice(1) : cat.toUpperCase()}`}</p>
+            </div>
+          ))}
         </div>
       </section>
       <section className="grid grid-cols-5 gap-x-32 gap-y-8">

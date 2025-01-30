73b4cd5ad36ca70ba57b9c751287e16c725b5740
tojamrcel <marcelgala42@gmail.com>
2025-01-30 18:04:20 +0100

    feat: implement availability status in product page

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -37,22 +37,35 @@ async function Page({ params }: { params: { productId: string } }) {
             {product.product_name}
           </h2>
           <p className="text-gray-700">{product.description}</p>
-          {product.discount ? (
-            <>
-              <div className="flex gap-2 text-xl">
-                <span className="italic text-gray-700 line-through">
-                  {product.regular_price}$
-                </span>
-                <span className="text-red-600">
-                  {product.regular_price - Number(product.discount)}$
-                </span>
-              </div>
-            </>
-          ) : (
-            <span className="text-xl font-bold text-gray-700">
-              {product.regular_price - Number(product.discount)}$
-            </span>
-          )}
+          <div className="flex flex-col">
+            {product.availability > 3 && (
+              <p className="text-green-700">Available</p>
+            )}
+            {product.availability === 0 && (
+              <p className="text-red-600">Unavailable</p>
+            )}
+            {product.availability > 0 && product.availability <= 3 && (
+              <p className="text-yellow-700">
+                Only {product.availability} in stock
+              </p>
+            )}
+            {product.discount ? (
+              <>
+                <div className="flex gap-2 text-xl">
+                  <span className="italic text-gray-700 line-through">
+                    {product.regular_price}$
+                  </span>
+                  <span className="text-red-600">
+                    {product.regular_price - Number(product.discount)}$
+                  </span>
+                </div>
+              </>
+            ) : (
+              <span className="text-xl font-bold text-gray-700">
+                {product.regular_price - Number(product.discount)}$
+              </span>
+            )}
+          </div>
 
           <div className="flex items-center gap-2">
             <AddToCartButton product={product} />

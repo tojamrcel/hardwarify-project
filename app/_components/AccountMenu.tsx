7b467402f543bdc8a8e8c3326924e05667af6353
tojamrcel <marcelgala42@gmail.com>
2025-01-30 17:49:32 +0100

    feat: add next Link component to AccountMenu component

--- app/_components/AccountMenu.tsx
+++ app/_components/AccountMenu.tsx
@@ -1,20 +1,28 @@
+import Link from "next/link";
+
 function AccountMenu() {
   return (
     <nav className="p-4">
       <ul className="flex flex-col gap-4 text-xl text-gray-700">
-        <li className="rounded-lg bg-gray-300 p-2 hover:bg-gray-300">
-          <a href="#" className="font-semibold">
-            General
-          </a>
+        <li className="rounded-lg bg-gray-300 p-2 text-gray-700 hover:bg-gray-300">
+          <Link href="/account" className="block font-semibold">
+            Account
+          </Link>
         </li>
         <li className="rounded-lg p-2 hover:bg-gray-300">
-          <a href="#">My orders</a>
+          <Link href="/account/orders" className="block">
+            My orders
+          </Link>
         </li>
         <li className="rounded-lg p-2 hover:bg-gray-300">
-          <a href="#">Previous orders</a>
+          <Link href="/account/pastorders" className="block">
+            Previous orders
+          </Link>
         </li>
         <li className="rounded-lg p-2 hover:bg-gray-300">
-          <a href="#">Settings</a>
+          <Link href="/account/settings" className="block">
+            Settings
+          </Link>
         </li>
       </ul>
     </nav>

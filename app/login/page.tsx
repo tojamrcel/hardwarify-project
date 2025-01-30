d2e41bde98b7bc50aef8edd7782d63884b72830d
tojamrcel <marcelgala42@gmail.com>
2025-01-30 18:38:40 +0100

    feat: create register and login page

---
+++ app/login/page.tsx
@@ -0,0 +1,10 @@
+function Page() {
+  return (
+    <div className="flex max-h-screen max-w-96 flex-col items-center">
+      <h2 className="text-4xl font-bold text-gray-700">Login</h2>
+      <form></form>
+    </div>
+  );
+}
+
+export default Page;

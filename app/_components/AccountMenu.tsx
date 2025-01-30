6ef543910bb4aac8a068db695dada2ff92a1be4d
tojamrcel <marcelgala42@gmail.com>
2025-01-30 17:55:28 +0100

    style: change styles for checkboxes in filtersg

--- app/_components/AccountMenu.tsx
+++ app/_components/AccountMenu.tsx
@@ -6,7 +6,7 @@ function AccountMenu() {
       <ul className="flex flex-col gap-4 text-xl text-gray-700">
         <li className="rounded-lg bg-gray-300 p-2 text-gray-700 hover:bg-gray-300">
           <Link href="/account" className="block font-semibold">
-            Account
+            General
           </Link>
         </li>
         <li className="rounded-lg p-2 hover:bg-gray-300">
--- app/_components/FilterCategory.tsx
+++ app/_components/FilterCategory.tsx
@@ -15,6 +15,7 @@ function FilterCategory({
         value={cat}
         onChange={() => handleFilters(cat)}
         checked={filters.includes(cat)}
+        className="h-4 w-4"
       />
       <p className="text-md text-gray-700">{`${cat.length > 3 ? cat[0].toUpperCase() + cat.slice(1) : cat.toUpperCase()}`}</p>
     </div>

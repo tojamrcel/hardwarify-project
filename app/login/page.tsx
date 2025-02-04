fdb2ba784cfc7fcfbd8ea88ed5aa8f773a96bf59
tojamrcel <marcelgala42@gmail.com>
2025-02-04 20:20:07 +0100

    feat: create template for login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,39 @@
 function Page() {
   return (
-    <div className="flex max-h-screen max-w-96 flex-col items-center">
+    <div className="flex flex-col items-center gap-8">
       <h2 className="text-4xl font-bold text-gray-700">Login</h2>
-      <form></form>
+      <form className="flex flex-col items-center gap-2">
+        <div className="flex flex-col gap-1">
+          <label
+            htmlFor="email"
+            className="text-md font-semibold text-gray-500"
+          >
+            Login
+          </label>
+          <input
+            type="email"
+            name="email"
+            className="text-md h-10 w-96 rounded-md p-2 text-center text-gray-800 shadow-sm outline-none transition-all duration-200 focus:shadow-lg"
+            placeholder="jankowalski@mail.com"
+          />
+        </div>
+        <div className="flex flex-col gap-1">
+          <label
+            htmlFor="email"
+            className="text-md font-semibold text-gray-500"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            className="text-md h-10 w-96 rounded-md p-2 text-center text-gray-800 shadow-sm outline-none transition-all duration-200 focus:shadow-lg"
+          />
+        </div>
+        <button className="ml-auto mt-2 rounded-md bg-red-600 px-4 py-2 font-semibold text-stone-100 transition-all duration-300 hover:bg-red-700">
+          Login
+        </button>
+      </form>
     </div>
   );
 }

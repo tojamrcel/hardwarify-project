a2ebe1b54bc6a427616b840aaa6dff0c2ec78f51
tojamrcel <marcelgala42@gmail.com>
2025-02-17 16:05:57 +0100

    fix: remove edit button from email row in acc settings

--- app/account/settings/page.tsx
+++ app/account/settings/page.tsx
@@ -22,14 +22,10 @@ function Page() {
         <input
           type="email"
           name="email"
-          className="text-md h-10 w-2/3 rounded-md p-2 text-center text-gray-800 shadow-sm outline-none transition-all duration-200 focus:shadow-lg read-only:focus:shadow-sm"
-          placeholder="jankowalski@mail.com"
+          className="text-md h-10 w-2/3 rounded-md p-2 text-center text-gray-400 shadow-sm outline-none transition-all duration-200"
+          value="jankowalski@mail.com"
           readOnly
         />
-        <button className="transition-color flex items-center justify-center gap-1 rounded-md bg-red-600 px-4 py-2 font-semibold text-stone-100 duration-300 hover:bg-red-700">
-          <HiPencil className="text-xl" />
-          Edit
-        </button>
       </div>
       <div className="flex gap-2">
         <input

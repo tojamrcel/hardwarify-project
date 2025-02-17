b41defcd174149f8eefc640fa9412b58e804954a
tojamrcel <marcelgala42@gmail.com>
2025-02-17 17:08:26 +0100

    feat: create thankyou page

---
+++ app/thankyou/page.tsx
@@ -0,0 +1,20 @@
+import Button from "../_components/Button";
+
+function Page() {
+  return (
+    <div className="mt-16 flex w-full flex-col items-center justify-center gap-4">
+      <h1 className="text-center text-5xl font-bold text-gray-700">
+        Thank you!
+      </h1>
+      <p className="mb-2 text-xl text-gray-600">
+        Thank you for placing the order in our shop. You can track your order
+        <span className="font-semibold"> #XXX</span> in account page!
+      </p>
+      <Button type="primary" link="/">
+        Go to shop
+      </Button>
+    </div>
+  );
+}
+
+export default Page;

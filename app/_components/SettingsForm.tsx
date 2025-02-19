f795d4fe329e8a6382d945410c2f9789eb052c02
tojamrcel <marcelgala42@gmail.com>
2025-02-19 10:45:40 +0100

    feat: implement state to handle displaying changing password form

--- app/_components/SettingsForm.tsx
+++ app/_components/SettingsForm.tsx
@@ -6,6 +6,14 @@ import { HiPencil } from "react-icons/hi";
 function SettingsForm({ email }: { email: string }) {
   const [isEditing, setIsEditing] = useState(false);
 
+  function handleChange() {
+    setIsEditing(true);
+  }
+
+  function handleClose() {
+    setIsEditing(false);
+  }
+
   return (
     <>
       <div className="flex gap-2">
@@ -25,20 +33,38 @@ function SettingsForm({ email }: { email: string }) {
           value="password"
           readOnly
         />
-        <button className="transition-color flex items-center justify-center gap-1 rounded-md bg-red-600 px-4 py-2 font-semibold text-stone-100 duration-300 hover:bg-red-700">
-          <HiPencil className="text-xl" />
-          Change password
-        </button>
+        {!isEditing && (
+          <button
+            onClick={handleChange}
+            className="transition-color flex items-center justify-center gap-1 rounded-md bg-red-600 px-4 py-2 font-semibold text-stone-100 duration-300 hover:bg-red-700"
+          >
+            <HiPencil className="text-xl" />
+            Change password
+          </button>
+        )}
       </div>
-      <div className="flex gap-2">
-        <input
-          type="password"
-          name="confirmpassword"
-          className="text-md h-10 w-2/3 rounded-md p-2 text-center text-gray-800 shadow-sm outline-none transition-all duration-200 focus:shadow-lg read-only:focus:shadow-sm"
-          placeholder="Confirm your password"
-          readOnly
-        />
-      </div>
+      {isEditing && (
+        <>
+          <div className="flex gap-2">
+            <input
+              type="password"
+              name="confirmpassword"
+              className="text-md h-10 w-2/3 rounded-md p-2 text-center text-gray-800 shadow-sm outline-none transition-all duration-200 focus:shadow-lg read-only:focus:shadow-sm"
+              placeholder="Confirm your password"
+              readOnly
+            />
+          </div>
+          <button className="transition-color flex w-2/3 items-center justify-center gap-1 rounded-md bg-red-600 px-4 py-2 font-semibold text-stone-100 duration-300 hover:bg-red-700">
+            CHANGE
+          </button>
+          <button
+            onClick={handleClose}
+            className="-mt-2 w-2/3 text-gray-600 hover:text-gray-800"
+          >
+            CLOSE
+          </button>
+        </>
+      )}
     </>
   );
 }

d19ec80ee0aa418c19a8026e1c7857cae597e63d
tojamrcel <marcelgala42@gmail.com>
2025-02-20 20:42:23 +0100

    feat: implement real cart items in summary in checkout page

--- app/_components/SummaryProductItem.tsx
+++ app/_components/SummaryProductItem.tsx
@@ -1,14 +1,19 @@
-function SummaryProductItem() {
+import Image from "next/image";
+import { CartProduct } from "../_types/types";
+
+function SummaryProductItem({ item }: { item: CartProduct }) {
   return (
     <div className="grid w-full flex-shrink-0 grid-cols-[8rem_3fr] overflow-hidden rounded-lg bg-white-second">
-      <img src="/person.jpg" width="96" height="96" alt="" className="" />
+      <Image src={item.image} width="96" height="96" alt={item.product_name} />
       <div className="flex h-full w-full items-center justify-between pr-4">
         <p className="font-semibold uppercase tracking-tighter text-gray-600">
-          product name
+          {item.product_name}
         </p>
         <div className="flex flex-col justify-center text-center font-semibold">
-          <p className="text-red-600">2x</p>
-          <p className="text-gray-600">399$</p>
+          <p className="text-red-600">{item.quantity}x</p>
+          <p className="text-gray-600">
+            {(item.regular_price - Number(item.discount)) * item.quantity}$
+          </p>
         </div>
       </div>
     </div>
--- app/_components/SummaryProducts.tsx
+++ app/_components/SummaryProducts.tsx
@@ -1,9 +1,14 @@
+import { useCart } from "./CartContext";
 import SummaryProductItem from "./SummaryProductItem";
 
 function SummaryProducts() {
+  const { cart } = useCart();
+
   return (
     <div className="flex max-h-[65%] w-full flex-col items-start justify-start gap-2 overflow-auto px-4 py-4">
-      <SummaryProductItem />
+      {cart.map((prod) => (
+        <SummaryProductItem item={prod} key={prod.id} />
+      ))}
     </div>
   );
 }

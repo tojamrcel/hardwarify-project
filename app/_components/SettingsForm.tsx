c65e60322ab39f95406829c030260c0a63ba579c
tojamrcel <marcelgala42@gmail.com>
2025-02-20 16:28:16 +0100

    feat: remove feature of changing password

--- app/_components/SettingsForm.tsx
+++ app/_components/SettingsForm.tsx
@@ -1,106 +1,13 @@
-"use client";
-
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-import { HiPencil } from "react-icons/hi";
-import { UpdatePassword } from "../_types/types";
-import InputErrorMessage from "./InputErrorMessage";
-
 function SettingsForm({ email }: { email: string }) {
-  const [isEditing, setIsEditing] = useState(false);
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm<UpdatePassword>();
-
-  function handleChange() {
-    setIsEditing(true);
-  }
-
-  function handleClose() {
-    setIsEditing(false);
-  }
-
-  function onSubmit(data: UpdatePassword) {
-    console.log(data);
-  }
-
   return (
-    <>
-      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
-        <div className="flex items-center gap-2">
-          <input
-            type="email"
-            className="text-md h-10 w-2/3 rounded-md p-2 text-center text-gray-400 shadow-sm outline-none transition-all duration-200"
-            value={email}
-            readOnly
-          />
-        </div>
-        <div className="flex items-center gap-2">
-          <input
-            type="password"
-            className="text-md h-10 w-2/3 rounded-md p-2 text-center text-gray-800 shadow-sm outline-none transition-all duration-200 focus:shadow-lg read-only:focus:shadow-sm"
-            defaultValue={isEditing ? "" : "password"}
-            placeholder="Password"
-            readOnly={!isEditing}
-            {...register("password", {
-              required: "Password is required.",
-              minLength: {
-                value: 8,
-                message: "Password must be at least 8 characters.",
-              },
-            })}
-          />
-          {errors.password && (
-            <InputErrorMessage>{errors.password.message}</InputErrorMessage>
-          )}
-
-          {!isEditing && (
-            <button
-              onClick={handleChange}
-              className="transition-color flex items-center justify-center gap-1 rounded-md bg-red-600 px-4 py-2 font-semibold text-stone-100 duration-300 hover:bg-red-700"
-            >
-              <HiPencil className="text-xl" />
-              Change password
-            </button>
-          )}
-        </div>
-        {isEditing && (
-          <>
-            <div className="flex items-center gap-2">
-              <input
-                type="password"
-                className="text-md h-10 w-2/3 rounded-md p-2 text-center text-gray-800 shadow-sm outline-none transition-all duration-200 focus:shadow-lg read-only:focus:shadow-sm"
-                placeholder="Confirm your password"
-                {...register("confirmPassword", {
-                  required: "Please confirm your password.",
-                  validate: (val) =>
-                    watch("password") === val ||
-                    "Password have to be the same.",
-                })}
-              />
-              {errors.confirmPassword && (
-                <InputErrorMessage>
-                  {errors.confirmPassword.message}
-                </InputErrorMessage>
-              )}
-            </div>
-            <button className="transition-color flex w-2/3 items-center justify-center gap-1 rounded-md bg-red-600 px-4 py-2 font-semibold text-stone-100 duration-300 hover:bg-red-700">
-              CHANGE
-            </button>
-            <button
-              type="button"
-              onClick={handleClose}
-              className="-mt-2 w-2/3 text-gray-600 transition-all duration-300 hover:text-gray-800"
-            >
-              CLOSE
-            </button>
-          </>
-        )}
-      </form>
-    </>
+    <div className="flex items-center gap-2">
+      <input
+        type="email"
+        className="text-md h-10 w-2/3 rounded-md p-2 text-center text-gray-400 shadow-sm outline-none transition-all duration-200"
+        value={email}
+        readOnly
+      />
+    </div>
   );
 }
 
--- app/_lib/supabase.js
+++ app/_lib/supabase.js
@@ -3,4 +3,10 @@ import { createClient } from "@supabase/supabase-js";
 export const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_KEY,
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+    },
+  },
 );

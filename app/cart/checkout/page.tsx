73b6cf9b587b9620acbac3e566fe8ad67b359c12
tojamrcel <marcelgala42@gmail.com>
2025-02-22 12:46:55 +0100

    feat: implement react hook forms in checkout page

--- app/cart/checkout/page.tsx
+++ app/cart/checkout/page.tsx
@@ -6,8 +6,25 @@ import SummaryProducts from "@/app/_components/SummaryProducts";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { SHIPPING_COST } from "@/app/_lib/constants";
+import { useForm } from "react-hook-form";
+import InputErrorMessage from "@/app/_components/InputErrorMessage";
+
+interface OrderForm {
+  first_name: string;
+  last_name: string;
+  email: string;
+  city: string;
+  postal_code: string;
+  address: string;
+}
 
 function Page() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<OrderForm>();
+
   const { data } = useSession();
   const { cart } = useCart();
   const router = useRouter();
@@ -25,13 +42,17 @@ function Page() {
 
   const totalPrice = productsPrice - discount;
 
+  function onSubmit(data: OrderForm) {
+    console.log(data);
+  }
+
   return (
     <>
       <h2 className="text-4xl font-bold text-gray-700">
         Shipping and payment details
       </h2>
       <div className="mt-4 grid w-full grid-cols-[2fr_1.5fr] px-4">
-        <form className="flex flex-col gap-2">
+        <form className="flex flex-col gap-2" onSubmit={handleSubmit(onSubmit)}>
           <InputRow>
             <label
               htmlFor="first_name"
@@ -41,10 +62,19 @@ function Page() {
             </label>
             <input
               type="text"
-              name="first_name"
               className="text-md h-10 w-2/3 rounded-md p-2 text-gray-800 shadow-sm outline-none transition-all duration-200 focus:shadow-lg"
               placeholder="John"
+              {...register("first_name", {
+                required: "First name is required.",
+                minLength: {
+                  value: 3,
+                  message: "First name should be at least 3 characters.",
+                },
+              })}
             />
+            {errors.first_name && (
+              <InputErrorMessage>{errors.first_name.message}</InputErrorMessage>
+            )}
           </InputRow>
           <InputRow>
             <label
@@ -55,10 +85,19 @@ function Page() {
             </label>
             <input
               type="text"
-              name="last_name"
               className="text-md h-10 w-2/3 rounded-md p-2 text-gray-800 shadow-sm outline-none transition-all duration-200 focus:shadow-lg"
               placeholder="Kowalski"
+              {...register("last_name", {
+                required: "Last name is required.",
+                minLength: {
+                  value: 3,
+                  message: "Last name should be at least 3 characters.",
+                },
+              })}
             />
+            {errors.last_name && (
+              <InputErrorMessage>{errors.last_name.message}</InputErrorMessage>
+            )}
           </InputRow>
           <InputRow>
             <label
@@ -69,10 +108,15 @@ function Page() {
             </label>
             <input
               type="email"
-              name="email"
               className="text-md h-10 w-2/3 rounded-md p-2 text-gray-700 shadow-sm outline-none transition-all duration-200 focus:shadow-lg"
               defaultValue={data?.user?.email ? data.user.email : ""}
+              {...register("email", {
+                required: "Email is required.",
+              })}
             />
+            {errors.email && (
+              <InputErrorMessage>{errors.email.message}</InputErrorMessage>
+            )}
           </InputRow>
           <InputRow>
             <label
@@ -83,10 +127,14 @@ function Page() {
             </label>
             <input
               type="text"
-              name="city"
               className="text-md h-10 w-2/3 rounded-md p-2 text-gray-800 shadow-sm outline-none transition-all duration-200 focus:shadow-lg"
+              {...register("city", { required: "City is required." })}
             />
+            {errors.city && (
+              <InputErrorMessage>{errors.city.message}</InputErrorMessage>
+            )}
           </InputRow>
+
           <InputRow>
             <label
               htmlFor="postal_code"
@@ -96,10 +144,18 @@ function Page() {
             </label>
             <input
               type="text"
-              name="postal_code"
               className="text-md h-10 w-2/3 rounded-md p-2 text-gray-800 shadow-sm outline-none transition-all duration-200 focus:shadow-lg"
+              {...register("postal_code", {
+                required: "Postal code is required.",
+              })}
             />
+            {errors.postal_code && (
+              <InputErrorMessage>
+                {errors.postal_code.message}
+              </InputErrorMessage>
+            )}
           </InputRow>
+
           <InputRow>
             <label
               htmlFor="address"
@@ -109,9 +165,12 @@ function Page() {
             </label>
             <input
               type="text"
-              name="address"
               className="text-md h-10 w-2/3 rounded-md p-2 text-gray-800 shadow-sm outline-none transition-all duration-200 focus:shadow-lg"
+              {...register("address", { required: "Address is required." })}
             />
+            {errors.address && (
+              <InputErrorMessage>{errors.address.message}</InputErrorMessage>
+            )}
           </InputRow>
           <button className="mt-2 self-start rounded-md bg-red-600 px-4 py-2 font-semibold text-stone-100 transition-all duration-300 hover:bg-red-700">
             Order

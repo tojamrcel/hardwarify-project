ea6c635273753e68bbd803a001b19520180b58d2
tojamrcel <marcelgala42@gmail.com>
2025-02-28 11:46:05 +0100

    feat: implement filter to display only sent and pending orders in orders page

--- app/account/orders/page.tsx
+++ app/account/orders/page.tsx
@@ -2,7 +2,9 @@ import OrderItem from "@/app/_components/OrderItem";
 import { getUserOrders } from "@/app/_lib/data_service";
 
 async function Page() {
-  const orders = await getUserOrders();
+  const orders = (await getUserOrders()).filter(
+    (order) => order.status === "pending" || order.status === "sent",
+  );
 
   return (
     <ul className="flex flex-col gap-4">

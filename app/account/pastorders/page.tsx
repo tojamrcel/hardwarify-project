e036d0b28c10f94e79bfd1637129681940d419cc
tojamrcel <marcelgala42@gmail.com>
2025-02-28 11:47:32 +0100

    feat: implement real data in past orders page

--- app/account/pastorders/page.tsx
+++ app/account/pastorders/page.tsx
@@ -1,24 +1,16 @@
-function Page() {
+import OrderItem from "@/app/_components/OrderItem";
+import { getUserOrders } from "@/app/_lib/data_service";
+
+async function Page() {
+  const orders = (await getUserOrders()).filter(
+    (order) => order.status === "delivered",
+  );
+
   return (
     <ul className="flex flex-col gap-4">
-      <li className="relative cursor-default rounded-lg bg-white-second p-4 shadow-md transition-transform duration-300 hover:scale-[1.01]">
-        <div className="flex items-center gap-4">
-          <h3 className="text-lg font-semibold text-gray-700">
-            Order #xxxxxxxxxx
-          </h3>
-          <span className="text-md flex rounded-full bg-red-500 p-0.5 px-1.5 font-semibold uppercase tracking-tight text-stone-100">
-            delivered
-          </span>
-        </div>
-        <div className="mt-2 flex gap-3">
-          <img src="/person.jpg" alt="" className="h-20 w-20 rounded-md" />
-          <img src="/person.jpg" alt="" className="h-20 w-20 rounded-md" />
-          <img src="/person.jpg" alt="" className="h-20 w-20 rounded-md" />
-        </div>
-        <button className="absolute bottom-3 right-5 border-b-2 border-transparent px-0.5 pb-[-0.25rem] pt-1 font-semibold text-stone-500 transition-colors duration-300 hover:border-stone-500">
-          Go to order
-        </button>
-      </li>
+      {orders.map((order) => (
+        <OrderItem orderItem={order} key={order.id} />
+      ))}
     </ul>
   );
 }

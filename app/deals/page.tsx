e5fff64f686435c0bb71ede43da65351142e8941
tojamrcel <marcelgala42@gmail.com>
2025-03-06 17:06:40 +0100

    style: improve responsiveness in deals page

--- app/deals/page.tsx
+++ app/deals/page.tsx
@@ -24,18 +24,18 @@ async function Page() {
 
   return (
     <>
-      <h2 className="mb-2 py-2 text-center text-5xl font-bold uppercase tracking-wider text-red-600 underline">
+      <h2 className="mb-2 py-2 text-center text-3xl font-bold uppercase tracking-wider text-red-600 underline md:text-4xl lg:text-5xl">
         HOT DEALS
       </h2>
       <section className="flex flex-col items-center rounded-lg bg-white-second py-4">
-        <h3 className="text-3xl font-bold text-gray-700">{name}</h3>
+        <h3 className="text-2xl font-bold text-gray-700 md:text-3xl">{name}</h3>
         <Image
           src={image}
           alt="Samsung"
           width={384}
           height={384}
           quality={95}
-          className="my-4 rounded-full"
+          className="my-4 w-64 rounded-full md:w-72 lg:w-auto"
         />
         <div className="mb-4 flex gap-2 text-xl">
           <span className="italic text-gray-700 line-through">

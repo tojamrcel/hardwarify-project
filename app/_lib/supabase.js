7455ba70616bca28dd6bf8ee4c41ac192e7a721d
tojamrcel <marcelgala42@gmail.com>
2025-03-11 14:56:06 +0100

    fix: fix auth bugs

--- app/_lib/supabase.js
+++ app/_lib/supabase.js
@@ -3,8 +3,8 @@ import { createClient } from "@supabase/supabase-js";
 export const supabaseUrl = "https://oluekkawuggalugtvbig.supabase.co";
 
 export const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY,
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   {
     auth: {
       persistSession: true,

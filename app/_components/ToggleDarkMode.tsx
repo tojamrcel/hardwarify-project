976e22b35fe73609372b289cfd84d5c6890eafd2
Marcel <marcel@macbook-air-marcel.home>
2025-08-26 20:02:48 +0200

    fix: fix dark mode, implement it with next-themes

--- app/_components/ToggleDarkMode.tsx
+++ app/_components/ToggleDarkMode.tsx
@@ -1,23 +1,33 @@
 "use client";
 
-import { useDarkMode } from "./DarkModeContext";
+import { useTheme } from "next-themes";
 import { MdOutlineDarkMode, MdOutlineLightMode } from "react-icons/md";
 
 function ToggleDarkMode() {
-  const { isDarkMode, setIsDarkMode } = useDarkMode()!;
-
-  function handleToggle() {
-    setIsDarkMode((dark) => !dark);
-  }
+  const { setTheme, theme } = useTheme();
+  console.log(theme);
 
   return (
-    <button
-      onClick={handleToggle}
-      className="flex items-center justify-center rounded-full text-3xl"
-    >
-      {isDarkMode && <MdOutlineLightMode />}
-      {!isDarkMode && <MdOutlineDarkMode />}
-    </button>
+    <>
+      <button
+        onClick={() => {
+          if (theme === "dark") setTheme("light");
+          if (theme === "light") setTheme("dark");
+        }}
+        className="flex items-center justify-center rounded-full text-3xl dark:hidden"
+      >
+        <MdOutlineDarkMode className="scale-100 dark:scale-0" />
+      </button>
+      <button
+        onClick={() => {
+          if (theme === "dark") setTheme("light");
+          if (theme === "light") setTheme("dark");
+        }}
+        className="hidden items-center justify-center rounded-full text-3xl dark:flex"
+      >
+        <MdOutlineLightMode />
+      </button>
+    </>
   );
 }
 

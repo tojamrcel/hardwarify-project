81f526d907eef3742704c871d75804cd9db13a7e
Marcel <marcel@macbook-air-marcel-1.home>
2025-10-07 21:03:14 +0200

    style: implement new mobile filters design

--- app/_components/Filters.tsx
+++ app/_components/Filters.tsx
@@ -9,6 +9,7 @@ import PriceFilter from "./PriceFilter";
 import { MAX_PRICE } from "../_lib/constants";
 import Button from "./Button";
 import { FiFilter } from "react-icons/fi";
+import { IoClose } from "react-icons/io5";
 
 interface FiltersProps {
   filters: FiltersType;
@@ -168,11 +169,76 @@ function Filters({ filters }: FiltersProps) {
         </Button>
       </div>
       {/* MOBILE */}
-      {isMobileFiltersOpen && (
-        <section className="absolute left-0 top-0 z-50 h-screen w-3/4 bg-gray-50 shadow-md backdrop-blur-lg md:w-1/2">
-          test
-        </section>
-      )}
+      <section
+        className={`fixed p-8 lg:hidden ${isMobileFiltersOpen ? "translate-x-0" : "-translate-x-[999px]"} left-0 top-0 z-[51] h-dvh w-3/4 -translate-x-[999px] bg-gray-50 shadow-md backdrop-blur-lg transition-all duration-150 md:w-1/2`}
+      >
+        <div className="mb-4 flex justify-between">
+          <h2 className="text-2xl font-semibold text-gray-700">Filters</h2>
+          <button
+            className="flex items-center justify-center text-3xl text-gray-700"
+            onClick={() => setIsMobileFiltersOpen(false)}
+          >
+            <IoClose />
+          </button>
+        </div>
+        <div className="flex flex-col gap-4 overflow-auto">
+          <div className="flex flex-col gap-2">
+            <label
+              htmlFor="brand"
+              className="text-lg font-semibold text-gray-600 dark:text-gray-300"
+            >
+              Brand
+            </label>
+            <div className="flex w-full flex-col items-center gap-1 px-2 lg:items-start">
+              {brands?.map((brand) => (
+                <FiltersItem
+                  filterItem={brand}
+                  key={brand}
+                  filters={filtersState}
+                  handleFilters={handleFilters}
+                />
+              ))}
+            </div>
+          </div>
+          <div className="flex flex-col gap-2">
+            <label
+              htmlFor="category"
+              className="text-lg font-semibold text-gray-600 dark:text-gray-300"
+            >
+              Category
+            </label>
+            <div className="flex w-full flex-col items-center gap-1 px-2 lg:items-start">
+              {categories?.map((cat) => (
+                <FiltersItem
+                  filterItem={cat}
+                  key={cat}
+                  filters={filtersState}
+                  handleFilters={handleFilters}
+                />
+              ))}
+            </div>
+          </div>
+          <div className="flex flex-col gap-2">
+            <label
+              htmlFor="price"
+              className="text-lg font-semibold text-gray-600 dark:text-gray-300"
+            >
+              Price
+            </label>
+            <div className="flex w-full flex-col items-center gap-1 px-2 lg:items-start">
+              <PriceFilter
+                price={
+                  filtersState["price"]
+                    ? filtersState.price
+                    : { min: 0, max: MAX_PRICE }
+                }
+                handleFilters={handlePriceFilter}
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+
       {/* DESKTOP */}
       <section className="hidden w-full flex-col items-center justify-self-stretch rounded-md border-2 p-2 px-6 dark:border-gray-700 md:w-3/4 lg:block lg:w-2/6 lg:self-stretch lg:p-6">
         <div className="flex flex-col gap-4">

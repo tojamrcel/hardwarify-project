3bc25e070f2acdedebcf334fa980f02dd8b55d53
tojamrcel <marcelgala42@gmail.com>
2025-10-22 23:58:44 +0200

    style: change styling in login and signup page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -39,62 +39,66 @@ function Page() {
   }
 
   return (
-    <div className="mt-8 flex flex-col items-center gap-8 lg:mt-16">
-      <h2 className="text-3xl font-bold text-gray-700 dark:text-gray-300 sm:text-4xl">
-        Login
-      </h2>
-      <form
-        className="flex flex-col items-center gap-2"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <InputRow>
-          <label
-            htmlFor="email"
-            className="text-md font-semibold text-gray-500"
-          >
-            Login
-          </label>
-          <input
-            type="email"
-            className="text-md h-10 w-80 rounded-md border-2 border-transparent p-2 text-center text-gray-800 shadow-sm outline-none transition-all duration-200 focus:border-red-700 focus:shadow-lg dark:bg-gray-700 dark:text-gray-200"
-            placeholder="jankowalski@mail.com"
-            {...register("email", { required: "Email is required." })}
-          />
-          {errors.email && (
-            <span className="text-sm text-red-600">{errors.email.message}</span>
-          )}
-        </InputRow>
-        <InputRow>
-          <label
-            htmlFor="password"
-            className="text-md font-semibold text-gray-500"
-          >
-            Password
-          </label>
-          <input
-            type="password"
-            className="text-md h-10 w-80 rounded-md border-2 border-transparent p-2 text-center text-gray-800 shadow-sm outline-none transition-all duration-200 focus:border-red-700 focus:shadow-lg dark:bg-gray-700 dark:text-gray-200"
-            {...register("password", { required: "Password is required." })}
-          />
-          {errors.password && (
-            <span className="text-sm text-red-600">
-              {errors.password.message}
-            </span>
-          )}
-        </InputRow>
-        {error && <span className="text-sm text-red-600">{error}</span>}
-        <div className="flex w-full items-center justify-between">
-          <Link
-            href="/signup"
-            className="underline-offset-3 border-gray-500 text-sm font-semibold text-gray-500 hover:underline"
-          >
-            I don&apos;t have an account yet
-          </Link>
-          <Button type="primary" disabled={isPending}>
-            Login
-          </Button>
-        </div>
-      </form>
+    <div className="mt-8 flex flex-col items-center gap-8 md:mt-32 lg:mt-64">
+      <div className="flex h-96 w-96 flex-col items-center justify-center gap-4 rounded-md p-8 dark:border-gray-700 dark:bg-gray-900/90 md:border md:bg-gray-100">
+        <h2 className="text-center text-3xl font-bold text-gray-700 dark:text-gray-300 sm:text-4xl">
+          Login
+        </h2>
+        <form
+          className="flex flex-col items-center gap-2"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <InputRow>
+            <label
+              htmlFor="email"
+              className="text-md font-semibold text-gray-500"
+            >
+              Login
+            </label>
+            <input
+              type="email"
+              className="text-md h-10 w-80 rounded-md border-2 border-transparent p-2 text-center text-gray-800 shadow-sm outline-none transition-all duration-200 focus:border-red-700 focus:shadow-lg dark:bg-gray-700 dark:text-gray-200"
+              placeholder="jankowalski@mail.com"
+              {...register("email", { required: "Email is required." })}
+            />
+            {errors.email && (
+              <span className="text-sm text-red-600">
+                {errors.email.message}
+              </span>
+            )}
+          </InputRow>
+          <InputRow>
+            <label
+              htmlFor="password"
+              className="text-md font-semibold text-gray-500"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              className="text-md h-10 w-80 rounded-md border-2 border-transparent p-2 text-center text-gray-800 shadow-sm outline-none transition-all duration-200 focus:border-red-700 focus:shadow-lg dark:bg-gray-700 dark:text-gray-200"
+              {...register("password", { required: "Password is required." })}
+            />
+            {errors.password && (
+              <span className="text-sm text-red-600">
+                {errors.password.message}
+              </span>
+            )}
+          </InputRow>
+          {error && <span className="text-sm text-red-600">{error}</span>}
+          <div className="flex w-full items-center justify-between">
+            <Link
+              href="/signup"
+              className="underline-offset-3 border-gray-500 text-sm font-semibold text-gray-500 hover:underline"
+            >
+              I don&apos;t have an account yet
+            </Link>
+            <Button type="primary" disabled={isPending}>
+              Login
+            </Button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -27,134 +27,136 @@ function Page() {
   }
 
   return (
-    <div className="mt-8 flex flex-col items-center gap-4 md:gap-8 lg:mt-16">
-      <h2 className="text-3xl font-bold text-gray-700 sm:text-4xl dark:text-gray-300">
-        Create new account
-      </h2>
-      <form
-        className="flex flex-col items-center gap-2"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <InputRow>
-          <label
-            htmlFor="email"
-            className="text-md font-semibold text-gray-500"
-          >
-            Email
-          </label>
-          <input
-            type="email"
-            className="text-md h-10 w-80 rounded-md border-2 border-transparent p-2 text-center text-gray-800 shadow-sm outline-none transition-all duration-200 focus:border-red-700 focus:shadow-lg dark:bg-gray-700 dark:text-gray-200"
-            placeholder="jankowalski@mail.com"
-            {...register("email", { required: "Email is required." })}
-          />
-          {errors.email && (
-            <InputErrorMessage>{errors.email.message}</InputErrorMessage>
-          )}
-        </InputRow>
-        <InputRow>
-          <label
-            htmlFor="firstName"
-            className="text-md font-semibold text-gray-500"
-          >
-            First name
-          </label>
-          <input
-            type="text"
-            className="text-md h-10 w-80 rounded-md border-2 border-transparent p-2 text-center text-gray-800 shadow-sm outline-none transition-all duration-200 focus:border-red-700 focus:shadow-lg dark:bg-gray-700 dark:text-gray-200"
-            placeholder="John"
-            {...register("firstName", {
-              required: "First name is required.",
-              minLength: {
-                value: 3,
-                message: "First name should be at least 3 characters.",
-              },
-            })}
-          />
-          {errors.firstName && (
-            <InputErrorMessage>{errors.firstName.message}</InputErrorMessage>
-          )}
-        </InputRow>
-        <InputRow>
-          <label
-            htmlFor="lastName"
-            className="text-md font-semibold text-gray-500"
-          >
-            Last name
-          </label>
-          <input
-            type="text"
-            className="text-md h-10 w-80 rounded-md border-2 border-transparent p-2 text-center text-gray-800 shadow-sm outline-none transition-all duration-200 focus:border-red-700 focus:shadow-lg dark:bg-gray-700 dark:text-gray-200"
-            placeholder="Kowalski"
-            {...register("lastName", {
-              required: "Last name is required.",
-              minLength: {
-                value: 3,
-                message: "Last name should be at least 3 characters.",
-              },
-            })}
-          />
-          {errors.lastName && (
-            <InputErrorMessage>{errors.lastName.message}</InputErrorMessage>
-          )}
-        </InputRow>
-        <InputRow>
-          <label
-            htmlFor="password"
-            className="text-md font-semibold text-gray-500"
-          >
-            Password
-          </label>
-          <input
-            type="password"
-            className="text-md h-10 w-80 rounded-md border-2 border-transparent p-2 text-center text-gray-800 shadow-sm outline-none transition-all duration-200 focus:border-red-700 focus:shadow-lg dark:bg-gray-700 dark:text-gray-200"
-            {...register("password", {
-              required: "Password is required",
-              minLength: {
-                value: 8,
-                message: "Password must be at least 8 characters.",
-              },
-            })}
-          />
-          {errors.password && (
-            <InputErrorMessage>{errors.password.message}</InputErrorMessage>
-          )}
-        </InputRow>
-        <InputRow>
-          <label
-            htmlFor="confirmpassword"
-            className="text-md font-semibold text-gray-500"
-          >
-            Confirm password
-          </label>
-          <input
-            type="password"
-            className="text-md h-10 w-80 rounded-md border-2 border-transparent p-2 text-center text-gray-800 shadow-sm outline-none transition-all duration-200 focus:border-red-700 focus:shadow-lg dark:bg-gray-700 dark:text-gray-200"
-            {...register("confirmPassword", {
-              required: "Please confirm your password.",
-              validate: (val) =>
-                val === watch("password") || "Passwords have to be the same.",
-            })}
-          />
-          {errors.confirmPassword && (
-            <InputErrorMessage>
-              {errors.confirmPassword.message}
-            </InputErrorMessage>
-          )}
-        </InputRow>
-        <div className="flex w-full items-center justify-between">
-          <Link
-            href="/login"
-            className="underline-offset-3 border-gray-500 text-sm font-semibold text-gray-500 hover:underline"
-          >
-            I already have an account
-          </Link>
-          <div>
-            <Button type="primary">Sign Up</Button>
+    <div className="mt-8 flex flex-col items-center gap-4 md:mt-32 md:gap-8 lg:mt-64">
+      <div className="flex flex-col gap-6 rounded-md border bg-gray-100 p-16 dark:border-gray-700 dark:bg-gray-900/90">
+        <h2 className="text-3xl font-bold text-gray-700 dark:text-gray-300 sm:text-4xl">
+          Create new account
+        </h2>
+        <form
+          className="flex flex-col items-center gap-2"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <InputRow>
+            <label
+              htmlFor="email"
+              className="text-md font-semibold text-gray-500"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              className="text-md h-10 w-80 rounded-md border-2 border-transparent p-2 text-center text-gray-800 shadow-sm outline-none transition-all duration-200 focus:border-red-700 focus:shadow-lg dark:bg-gray-700 dark:text-gray-200"
+              placeholder="jankowalski@mail.com"
+              {...register("email", { required: "Email is required." })}
+            />
+            {errors.email && (
+              <InputErrorMessage>{errors.email.message}</InputErrorMessage>
+            )}
+          </InputRow>
+          <InputRow>
+            <label
+              htmlFor="firstName"
+              className="text-md font-semibold text-gray-500"
+            >
+              First name
+            </label>
+            <input
+              type="text"
+              className="text-md h-10 w-80 rounded-md border-2 border-transparent p-2 text-center text-gray-800 shadow-sm outline-none transition-all duration-200 focus:border-red-700 focus:shadow-lg dark:bg-gray-700 dark:text-gray-200"
+              placeholder="John"
+              {...register("firstName", {
+                required: "First name is required.",
+                minLength: {
+                  value: 3,
+                  message: "First name should be at least 3 characters.",
+                },
+              })}
+            />
+            {errors.firstName && (
+              <InputErrorMessage>{errors.firstName.message}</InputErrorMessage>
+            )}
+          </InputRow>
+          <InputRow>
+            <label
+              htmlFor="lastName"
+              className="text-md font-semibold text-gray-500"
+            >
+              Last name
+            </label>
+            <input
+              type="text"
+              className="text-md h-10 w-80 rounded-md border-2 border-transparent p-2 text-center text-gray-800 shadow-sm outline-none transition-all duration-200 focus:border-red-700 focus:shadow-lg dark:bg-gray-700 dark:text-gray-200"
+              placeholder="Kowalski"
+              {...register("lastName", {
+                required: "Last name is required.",
+                minLength: {
+                  value: 3,
+                  message: "Last name should be at least 3 characters.",
+                },
+              })}
+            />
+            {errors.lastName && (
+              <InputErrorMessage>{errors.lastName.message}</InputErrorMessage>
+            )}
+          </InputRow>
+          <InputRow>
+            <label
+              htmlFor="password"
+              className="text-md font-semibold text-gray-500"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              className="text-md h-10 w-80 rounded-md border-2 border-transparent p-2 text-center text-gray-800 shadow-sm outline-none transition-all duration-200 focus:border-red-700 focus:shadow-lg dark:bg-gray-700 dark:text-gray-200"
+              {...register("password", {
+                required: "Password is required",
+                minLength: {
+                  value: 8,
+                  message: "Password must be at least 8 characters.",
+                },
+              })}
+            />
+            {errors.password && (
+              <InputErrorMessage>{errors.password.message}</InputErrorMessage>
+            )}
+          </InputRow>
+          <InputRow>
+            <label
+              htmlFor="confirmpassword"
+              className="text-md font-semibold text-gray-500"
+            >
+              Confirm password
+            </label>
+            <input
+              type="password"
+              className="text-md h-10 w-80 rounded-md border-2 border-transparent p-2 text-center text-gray-800 shadow-sm outline-none transition-all duration-200 focus:border-red-700 focus:shadow-lg dark:bg-gray-700 dark:text-gray-200"
+              {...register("confirmPassword", {
+                required: "Please confirm your password.",
+                validate: (val) =>
+                  val === watch("password") || "Passwords have to be the same.",
+              })}
+            />
+            {errors.confirmPassword && (
+              <InputErrorMessage>
+                {errors.confirmPassword.message}
+              </InputErrorMessage>
+            )}
+          </InputRow>
+          <div className="flex w-full items-center justify-between">
+            <Link
+              href="/login"
+              className="underline-offset-3 border-gray-500 text-sm font-semibold text-gray-500 hover:underline"
+            >
+              I already have an account
+            </Link>
+            <div>
+              <Button type="primary">Sign Up</Button>
+            </div>
           </div>
-        </div>
-        {error && <p className="text-sm text-red-600">{error}</p>}
-      </form>
+          {error && <p className="text-sm text-red-600">{error}</p>}
+        </form>
+      </div>
     </div>
   );
 }

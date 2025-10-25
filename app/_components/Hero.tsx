54e487d7236d899961df874c37f43980d7abbe55
Marcel <marcel@MacBook-Air-Marcel.local>
2025-10-25 13:50:24 +0200

    style: change styling for hero section

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -7,24 +7,24 @@ function Hero() {
     <section className="flex justify-center bg-gray-200/90 dark:bg-gray-800">
       <div className="mx-4 grid w-full max-w-[1200px] grid-cols-1 place-items-center justify-center justify-items-center py-12 md:grid-cols-[1fr_1fr] md:gap-16 md:py-16">
         <div className="flex flex-col items-center gap-2">
-          <h2 className="px-2 text-center text-6xl font-bold text-gray-700 dark:text-gray-300 lg:text-7xl">
+          <h2 className="px-2 text-center text-5xl font-bold text-gray-700 dark:text-gray-300 sm:text-6xl lg:text-7xl">
             <span className="text-nowrap">SAVE UP TO</span> 50%
           </h2>
-          <p className="w-full text-nowrap text-center text-xl font-[600] text-gray-600 dark:text-gray-300 lg:text-2xl lg:font-normal">
+          <p className="w-full text-nowrap text-center text-lg font-[600] text-gray-600 dark:text-gray-300 sm:text-xl lg:text-2xl lg:font-normal">
             Limited time offers on best-selling items
           </p>
           <Button type="primary" link="/products">
             SEE OFFER
           </Button>
         </div>
-        <div className="mt-4 md:mt-0">
+        <div className="mt-4 flex w-full justify-center md:mt-0">
           <Image
             src={heroimg}
             width={400}
             height={400}
             placeholder="blur"
             alt="nintendo console"
-            className="rounded-lg shadow-md dark:grayscale-[10%] md:w-auto"
+            className="w-3/4 rounded-lg shadow-md dark:grayscale-[10%] md:w-auto"
           />
         </div>
       </div>

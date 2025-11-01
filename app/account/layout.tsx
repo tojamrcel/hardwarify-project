12e7cbe3dd6b6e022f138f20cb69fee2c73b2f54
tojamrcel <marcelgala42@gmail.com>
2025-11-01 16:04:10 +0100

    style: implement new design in profile page

--- app/account/layout.tsx
+++ app/account/layout.tsx
@@ -7,11 +7,11 @@ function Layout({
 }>) {
   return (
     <div className="h-[calc(100vh-70px)] overflow-clip">
-      <section className="grid h-full gap-4 md:grid-cols-[360px_1fr] lg:gap-16">
+      <section className="grid h-full md:grid-cols-[360px_1fr]">
         <div className="flex h-full justify-center bg-gray-100/90 dark:bg-gray-900/90">
           <AccountMenu />
         </div>
-        <div className="">{children}</div>
+        <div className="p-8">{children}</div>
       </section>
     </div>
   );

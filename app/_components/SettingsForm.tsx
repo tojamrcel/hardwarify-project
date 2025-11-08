d488f0c241ff512324897ee8491f1956dccdb7df
tojamrcel <marcelgala42@gmail.com>
2025-11-08 23:17:49 +0100

    feat: implement gathering information from new settings form

--- app/_components/SettingsForm.tsx
+++ app/_components/SettingsForm.tsx
@@ -1,13 +1,60 @@
-function SettingsForm({ email }: { email: string }) {
+"use client";
+
+import InputRow from "./InputRow";
+import Button from "./Button";
+import { useForm } from "react-hook-form";
+import { Profile } from "../_types/types";
+
+function SettingsForm({ profile }: { profile: Profile }) {
+  const { email, firstName, lastName } = profile;
+  const { register, handleSubmit } = useForm<{
+    first_name: string | undefined;
+    last_name: string | undefined;
+  }>();
+
+  async function onSubmit(data: {
+    first_name: string | undefined;
+    last_name: string | undefined;
+  }) {
+    console.log(data);
+  }
+
   return (
-    <div className="flex items-center gap-2">
-      <input
-        type="email"
-        className="text-md h-10 rounded-md p-2 text-center text-gray-400 shadow-sm outline-none transition-all duration-200 sm:w-96"
-        value={email}
-        readOnly
-      />
-    </div>
+    <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
+      <InputRow>
+        <label className="text-lg font-semibold text-gray-700">E-mail</label>
+        <input
+          disabled
+          defaultValue={email}
+          className="h-10 w-full rounded-md border-2 px-4 text-gray-600 transition-colors duration-200 placeholder:italic focus:border-red-700 focus:outline-none dark:border-gray-700 dark:bg-[#0e131f] dark:text-gray-300 dark:shadow-md focus:dark:border-red-800"
+        />
+      </InputRow>
+      <InputRow>
+        <label className="text-lg font-semibold text-gray-700">
+          First name
+        </label>
+        <input
+          className="h-10 w-full rounded-md border-2 px-4 text-gray-600 transition-colors duration-200 placeholder:italic focus:border-red-700 focus:outline-none dark:border-gray-700 dark:bg-[#0e131f] dark:text-gray-300 dark:shadow-md focus:dark:border-red-800"
+          {...register("first_name", {
+            required: false,
+          })}
+          defaultValue={firstName}
+        />
+      </InputRow>
+      <InputRow>
+        <label className="text-lg font-semibold text-gray-700">Last name</label>
+        <input
+          className="h-10 w-full rounded-md border-2 px-4 text-gray-600 transition-colors duration-200 placeholder:italic focus:border-red-700 focus:outline-none dark:border-gray-700 dark:bg-[#0e131f] dark:text-gray-300 dark:shadow-md focus:dark:border-red-800"
+          {...register("last_name", {
+            required: false,
+          })}
+          defaultValue={lastName}
+        />
+      </InputRow>
+      <div className="flex w-full justify-end">
+        <Button type="primary">Save</Button>
+      </div>
+    </form>
   );
 }
 
--- app/account/settings/page.tsx
+++ app/account/settings/page.tsx
@@ -1,4 +1,4 @@
-import Button from "@/app/_components/Button";
+import SettingsForm from "@/app/_components/SettingsForm";
 import { getProfile } from "@/app/_lib/data_service";
 import { createClient } from "@/app/_lib/supabase/server";
 
@@ -9,34 +9,14 @@ async function Page() {
   } = await supabase.auth.getUser();
 
   if (!user || !user.email) throw new Error("There is no user logged in.");
-  const { firstName, lastName, image } = await getProfile(user.email);
+  const profile = await getProfile(user.email);
 
   return (
     <div className="flex flex-col gap-8">
       <h2 className="text-3xl font-semibold text-gray-700 dark:text-gray-300">
         Settings
       </h2>
-      <form className="flex flex-col gap-4">
-        <div className="flex flex-col gap-2">
-          <label className="text-lg font-semibold text-gray-700">E-mail</label>
-          <input className="h-10 w-full rounded-md border-2 px-4 text-gray-600 transition-colors duration-200 placeholder:italic focus:border-red-700 focus:outline-none dark:border-gray-700 dark:bg-[#0e131f] dark:text-gray-300 dark:shadow-md focus:dark:border-red-800" />
-        </div>
-        <div className="flex flex-col gap-2">
-          <label className="text-lg font-semibold text-gray-700">
-            First name
-          </label>
-          <input className="h-10 w-full rounded-md border-2 px-4 text-gray-600 transition-colors duration-200 placeholder:italic focus:border-red-700 focus:outline-none dark:border-gray-700 dark:bg-[#0e131f] dark:text-gray-300 dark:shadow-md focus:dark:border-red-800" />
-        </div>
-        <div className="flex flex-col gap-2">
-          <label className="text-lg font-semibold text-gray-700">
-            Last name
-          </label>
-          <input className="h-10 w-full rounded-md border-2 px-4 text-gray-600 transition-colors duration-200 placeholder:italic focus:border-red-700 focus:outline-none dark:border-gray-700 dark:bg-[#0e131f] dark:text-gray-300 dark:shadow-md focus:dark:border-red-800" />
-        </div>
-        <div className="flex w-full justify-end">
-          <Button type="primary">Save</Button>
-        </div>
-      </form>
+      <SettingsForm profile={profile} />
     </div>
   );
 }
